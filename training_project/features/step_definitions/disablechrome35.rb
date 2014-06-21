65200ba003cf0bb34803671cb7219ca8454e6d38
machzqcq <pradeep@seleniumframework.com>
2014-06-21 11:27:04 -0400

    added stackoverflow url for ch35 warning msg

--- training_project/features/step_definitions/disablechrome35.rb
+++ training_project/features/step_definitions/disablechrome35.rb
@@ -1,4 +1,5 @@
 Given(/^I launch puppies site$/) do
+  # http://stackoverflow.com/questions/23771922/protractor-error-message-unsupported-command-line-flag-in-chrome/24231337#24231337
   # caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => ["test-type","whitelisted-ips=127.0.0.1" ]})
   @browser = Selenium::WebDriver.for :chrome
   #@browser =  Selenium::WebDriver.for :remote, :url => "http://192.168.211.1:4444/wd/hub",:desired_capabilities => caps
