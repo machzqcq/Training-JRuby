6ffce32e3ea637f87c6d56e70c6cabec5057d0db
machzqcq <pradeep@seleniumframework.com>
2014-08-05 19:32:58 -0400

    Database sqlite3 scenarios added

--- training_project/features/support/database.rb
+++ training_project/features/support/database.rb
@@ -6,5 +6,23 @@
 # @jdbc_wordpress_url = "jdbc:mysql://localhost:3306/cucumber"
 
 # Oracle connection string
-# Install 'activerecord-jdbcmysql-adapter' and "activerecord-oracle_enhanced-adapter", place the oracle client jar file in classpath
+# Install 'activerecord-jdbc-adapter' and "activerecord-oracle_enhanced-adapter", place the oracle client jar file in classpath
 # typically in jruby/lib
+# http://stackoverflow.com/questions/755207/how-to-connect-to-oracle-using-jruby-jdbc
+
+# full information : https://github.com/jruby/activerecord-jdbc-adapter
+# Connecting to DB2 using jruby is also simple
+# Get the db2 jar and put it in jruby/lib directory
+#
+# ActiveRecord::Base.establish_connection(
+#     :adapter => "jdbc",
+#     :driver => "com.ibm.db2.jcc.DB2Driver",
+#     :url => "jdbc::db2://<url>:<port>/<instance>",
+#     :username => "<username",
+#     :password => "<password",
+#     :schema => "<schema"
+#
+# )
+
+# Then create ruby classes for corresponding table names. From here active record behaves
+# the same way as code that runs behind sqlite3_database.feature
