9010333571993d85c39b12c28ba06e812773aef0
machzqcq <pradeep@seleniumframework.com>
2014-08-17 22:01:17 -0400

    minor modifications

--- training_project/features/step_definitions/check_select.rb
+++ training_project/features/step_definitions/check_select.rb
@@ -10,17 +10,6 @@ Then(/^I complete the adoption$/) do
   @browser.find_element(:class => 'rounded_button').click
 end
 
-Then(/^I select "([^"]*)" in select box by text$/) do |arg|
-  order_pay_type = @browser.find_element(:id => 'order_pay_type')
-  pay_type_select_list = Selenium::WebDriver::Support::Select.new(order_pay_type)
-  pay_type_select_list.select_by(:text, arg)
-end
-
-Then(/^I select "([^"]*)" in select box by value$/) do |arg|
-  order_pay_type = @browser.find_element(:id => 'order_pay_type')
-  pay_type_select_list = Selenium::WebDriver::Support::Select.new(order_pay_type)
-  pay_type_select_list.select_by(:value, arg)
-end
 
 Then(/^I click popular cards checkbox$/) do
   @browser.find_element(:id => 'cb-popular').click
